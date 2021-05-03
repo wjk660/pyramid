759780e46da4a1a4aedb1e50e48a5464fa7c9c2f
wangjiakang <wangjiakang@xiaomi.com>
2021-05-03 22:34:10 +0800

    [save testPic.py]

--- testPic.py
+++ testPic.py
@@ -15,79 +15,40 @@ import glob
 import os
 from os.path import isdir
 import shutil
+import ruihua
+import args
 
-# 调试注意：更改origin_path和bin_path、args.loadModelName
-origin_path="/home/wangjk/dataset/DenseLeaves/classification/origin"
-bin_path="/home/wangjk/dataset/DenseLeaves/classification/bin/epoch500"
-if not os.path.exists(origin_path):
-    raise("not folder or no picture")
-if not os.path.exists(bin_path):
-    os.mkdir(bin_path)
-# 执行代码前清空指定的文件夹
-shutil.rmtree(bin_path)
-os.mkdir(bin_path)
-#改名字
-# for jsonfile in origin_folders_paths:
-class args:
-    loadModelName="Epoch500Pathsize256allownpicNoRotationLaplacian"
-    load_model = f'/home/wangjk/project/pyramid/modelParameter/{loadModelName}.pt'
-    pic_name="测试专用.JPG"
-    pic_prefix=pic_name.split('.')[0]
-    image = f"/home/wangjk/project/pyramid/testPic/{pic_name}"
-    save_path = ""
-describe = f"epoch{train.args.epochs},patch_size = {train.args.patch_size}*{train.args.patch_size},add three pic that labeled all leaves"
-
-if __name__ == '__main__':
-    sys.stdout = Logger("log/custom_image.txt")
-    print(describe)
-    faulthandler.enable()  # 报错时能看到原因
-    # args = parse_args()
-    # args={"load_model":'pyramid_net_model.pt',"image":'DJI_0986.JPG',"save_path":""}
-
-    # todo totally arbitrary weights
-    model = PyramidNet(n_layers=5, loss_weights=[torch.tensor([1.0])]*5)#, torch.tensor([1.9]), torch.tensor([3.9]),
-    testRes = f"{args.image.split('/')[-1].split('.')[0]}_By_{args.load_model.split('/')[-1].split('.')[0]}"
-    # torch.tensor([8]), torch.tensor([10])])
-    # print(model.parameters())
-    device=torch.device("cuda:0")
-    model = model.to(device)
-    if args.load_model:
-        model.load_state_dict(torch.load(args.load_model))
+def test_picture(dirpath_orginpics,bin_outputPath,model,path_load_model,name_load_model):
+    if model:
+        print("has model")
     else:
-        print("Please provide a valid path to the pre-trained model to evaluate")
+        print("Please provide a valid model to evaluate")
         exit(1)
-
-
-    list_imgsPath = [image_test for image_test in glob.glob(origin_path + "/*.JPG")]  # 读出所有文件夹的路径
+    if path_load_model:
+        model.load_state_dict(torch.load(path_load_model))
+    else:
+        print("Please provide a valid path to evaluate")
+        exit(1)
+    model.eval()  # 设置为evaluation模式
+    list_imgsPath = [image_test for image_test in glob.glob(dirpath_orginpics + "/*.JPG")]  # 读出所有文件夹的路径
     for i in range(len(list_imgsPath)):
-        og_image = cv2.imread(list_imgsPath[i])
-        # if og_image.shape[0] > 1024 and og_image.shape[1]>1024:  # 从中间取1024*1024大小的图像
-        #     og_image = og_image[(og_image.shape[0]-1024)//2:og_image.shape[0]-(og_image.shape[0]-1024)//2, (og_image.shape[1]-1024)//2:og_image.shape[1]-(og_image.shape[1]-1024)//2, :]
-        # resize image or crop patches
-        # image = cv2.resize(og_image, (128, 128))
-        # og_image = cv2.cvtColor(og_image, cv2.COLOR_BGR2RGB)
-        # image = torch.from_numpy(og_image.transpose(2, 0, 1) / 255.).float()
-
-        # 增加灰度处理，转为单通道
-        # imgname = og_image
-        imggray = cv2.imread(list_imgsPath[i],0)
-        # plt.imshow(imggray, cmap='gray')
-        # plt.show()
-        imgLap = cv2.Laplacian(imggray, cv2.CV_16S)
-        # plt.imshow(imgLap)
-        # plt.show()
-        imgLapDelta = cv2.convertScaleAbs(imgLap)
-        # plt.imshow(imgLapDelta)
-        # plt.show()
-        # Laplace Add
-        imgLapAdd = cv2.addWeighted(imggray, 1.0, imgLap, -1.0, 0, dtype=cv2.CV_32F)
-        imgLapAdd = cv2.convertScaleAbs(imgLapAdd)
-        # print(image.shape)
-        imgLapAdd = cv2.cvtColor(imgLapAdd, cv2.COLOR_GRAY2RGB)
-        image = torch.from_numpy(imgLapAdd.transpose(2, 0, 1) / 255.).float()
-
+        image = cv2.imread(list_imgsPath[i])
+        if args.is_ruihua:
+            # 增加灰度处理，转为单通道
+            # imgname = og_image
+            imggray = cv2.imread(list_imgsPath[i],0)
+            imgLap = cv2.Laplacian(imggray, cv2.CV_16S)
+            imgLapDelta = cv2.convertScaleAbs(imgLap)
+            # Laplace Add
+            imgLapAdd = cv2.addWeighted(imggray, 1.0, imgLap, -1.0, 0, dtype=cv2.CV_32F)
+            imgLapAdd = cv2.convertScaleAbs(imgLapAdd)
+            # print(image.shape)
+            imgLapAdd = cv2.cvtColor(imgLapAdd, cv2.COLOR_GRAY2RGB)
+            image = torch.from_numpy(imgLapAdd.transpose(2, 0, 1) / 255.).float()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_opencv=image.copy()
+        image = torch.from_numpy(image.transpose(2, 0, 1) / 255.).float()
         image = image.to(device)
-        model.eval()  # 设置为evaluation模式
         with torch.no_grad():
             predictions = model(image.unsqueeze(0))
             # get prediction at max resolution predictions本身返回的是多个分辨率都有的，现在只保留最高的分辨率那个，因为实验表明这个最好
@@ -96,32 +57,50 @@ if __name__ == '__main__':
             p = (p > 0.).float() # 如果大于0，则置为1
             p = p.squeeze().cpu().numpy().astype(np.float32)  # p变成2维的了,值是 0或者1
             Name_binImg=list_imgsPath[i].split('/')[-1]
-            cv2.imwrite(os.path.join(bin_path,f'bin_{Name_binImg}' ), (p * 255).astype(np.uint8))
-            # cv2.imwrite(os.path.join(bin_path, f'bin_{args.pic_prefix}_{args.loadModelName}.png'), (p * 255).astype(np.uint8))
-            # cv2.imwrite(os.path.join(args.save_path, f'testPic/mix_{args.pic_prefix}_{train.saveModelName}.png'), (p.unsqueeze(0) * 255+og_image).astype(np.uint8))
-            # res=cv2.cvtColor(p,)
-            # mix = cv2.addWeighted(og_image, 0.7, (p * 255).astype(np.uint8), 0.3, 0)
-            # cv2.imshow('mix', mix)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
-            # print(p.shape)
-            # plt.figure()
-            # plt.subplot(2,2,1)
-            # plt.imshow(p)
-            # plt.subplot(2,2,2)
-            # # fix, ax = plt.subplots(1, 2)
-            #
-            # # ax[0].imshow(og_image)
-            # # ax[1].imshow(p, cmap='Greys')
-            #
-            # # plt.legend(['ax[0]','ax[1]'])
-            # plt.imshow(og_image)
-            # plt.subplot(2,2,3)
-            # 为了生成用于训练的图像把这段注释掉了
-            # plt.imshow(og_image,alpha=0.7)
-            # plt.imshow(p,alpha=0.3)
-            # plt.savefig(f"/home/wangjk/project/pyramid/testPic/mix_{args.pic_prefix}_{args.loadModelName}.png")
-            # plt.show()
+            res_binImg=(p * 255).astype(np.uint8)
+            cv2.imwrite(os.path.join(bin_outputPath,f'{name_load_model}-{Name_binImg}' ), res_binImg)
+            mask_inv=cv2.bitwise_not(res_binImg)
+            res_mixImg = cv2.bitwise_and(image_opencv,image_opencv, mask=mask_inv)
+            cv2.imwrite(os.path.join(bin_outputPath,f'{name_load_model}-mix-{Name_binImg}' ), res_mixImg)
+    print("finish testPic sucessfully!")
+    exit(0)
+if __name__ == '__main__':
+    # sys.stdout = Logger("log/custom_image.txt")
+    # print(describe)
+    faulthandler.enable()  # 报错时能看到原因
+    # 调试注意：更改origin_path和bin_path、args.loadModelName
+    origin_path = "/home/wangjk/dataset/DenseLeaves/classification/origin"
+    bin_path = "/home/wangjk/dataset/DenseLeaves/classification/bin"
+    if not os.path.exists(origin_path):
+        raise ("not folder or no picture")
+    if not os.path.exists(bin_path):
+        os.mkdir(bin_path)
+    # 执行代码前清空指定的文件夹
+    # shutil.rmtree(bin_path)
+    # os.mkdir(bin_path)
+
+    # 改名字
+    # for jsonfile in origin_folders_paths:
+    loadModelName = "04-28_09-37Size224Epoch199"
+    load_model = f'/home/wangjk/project/pyramid/modelParameter/{loadModelName}.pt'
+    # pic_name = "测试专用.JPG"
+    # pic_prefix = pic_name.split('.')[0]
+    # image = f"/home/wangjk/project/pyramid/testPic/{pic_name}"
+    # save_path = ""
+    describe = f"epoch{train.args.epochs},patch_size = {train.args.patch_size}*{train.args.patch_size},add three pic that labeled all leaves"
+
+    # todo totally arbitrary weights
+    model = PyramidNet(n_layers=5, loss_weights=[torch.tensor([1.0])]*5)#, torch.tensor([1.9]), torch.tensor([3.9]),
+    # model = PyramidNet(n_layers=5,
+    #                    loss_weights=[torch.tensor([0.2]), torch.tensor([0.4]), torch.tensor([0.6]), torch.tensor([0.8]),
+    #                                  torch.tensor([1.0])])  # , torch.tensor([1.1]), torch.tensor([1.8]),
+
+    device=torch.device("cuda:0")
+    model = model.to(device)
+    test_picture(origin_path,bin_path,model,load_model,loadModelName)
+
+
+
 
 
 
